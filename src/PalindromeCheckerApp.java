c0afc0bfd8d3f6498443851832cf1b1c7d9f3ddf
Thirukarthick06 <thirukarthick2006@gmail.com>
2026-02-19 11:10:40 +0530

    UC4: Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,20 +2,15 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Declare and initialize the input string
         String input = "radar";
 
-        // Convert the string into a character array
         char[] chars = input.toCharArray();
 
-        // Initialize pointers
         int start = 0;
         int end = chars.length - 1;
 
-        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Continue comparison until pointers cross
         while (start < end) {
             if (chars[start] != chars[end]) {
                 isPalindrome = false;
@@ -25,7 +20,6 @@ public class PalindromeCheckerApp {
             end--;
         }
 
-        // Display result
         System.out.println("Input text: " + input);
         System.out.println("Is Palindrome?: " + isPalindrome);
     }
